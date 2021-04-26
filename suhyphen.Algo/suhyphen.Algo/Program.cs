943416932b3f317c77a067c10c917f2c3b367898
Akhilesh Singh <akhi.singh.baghel@gmail.com>
2021-04-27 01:57:29 +0530

    Initial Commit

---
+++ suhyphen.Algo/suhyphen.Algo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace suhyphen.Algo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
